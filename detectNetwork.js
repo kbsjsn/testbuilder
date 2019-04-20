35c1e950180e211925f30bc0362e80327cc44fc4
kbsjsn <shin.kirk@gmail.com>
2019-04-20 16:16:25 -0700

    First edits

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,8 +1,8 @@
-// Given a credit card number, this function should return a string with the 
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
@@ -11,8 +11,21 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  var prefix = cardNumber.substring(0, 2);
+  var preFour = cardNumber.substring(0, 4);
+  if ((prefix == "38" || prefix == "39") && cardNumber.length == 14) {
+    return "Diner's Club";
+  } if ((prefix == "34" || prefix == "37") && cardNumber.length == 15) {
+    return "American Express";
+  } if (cardNumber.substring(0, 1) == "4" && (cardNumber.length == 13 || cardNumber.length == 16 || cardNumber.length == 19)) {
+    return "Visa";
+  } if ((prefix == "51" || prefix == "52" || prefix == "53" || prefix == "54" || prefix == "55") && cardNumber.length == 16) {
+    return "MasterCard";
+  } if (preFour == "6011" && (cardNumber.length == 16 || cardNumber.length == 19)) {
+    return "Discover";
+  } if ((preFour == '5018' || preFour == '5020' || preFour == '5038' || preFour == '6304') && cardNumber.length >= 12 && cardNumber.length <= 19) {
+    return "Maestro";
+  }
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
-
-
